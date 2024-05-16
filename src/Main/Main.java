dbcc0b5d212e147f24a7f52811846ef66761cf63
Joseba <t0k3r0@gmail.com>
2024-05-16 17:35:53 +0200

    first commit

---
+++ src/Main/Main.java
@@ -0,0 +1,14 @@
+package Main;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Introduce tu nombre\n");
+		String nombre = sc.next();
+		System.out.print("hola " + nombre + ", perteneces a la generación del futuro");
+	}
+
+}
